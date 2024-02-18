c41d47e05cf73bdd862f1bdfe7f859b20382041f
tariik12 <tariikul12@gmail.com>
2024-02-18 16:20:02 +0600

    learn python on the w3 School

---
+++ helloworld.py
@@ -0,0 +1,73 @@
+print("hello World")  
+
+# This is a comment
+if 5 > 2 :
+    print("Five is greater than tow")
+
+
+'''
+hello world I want to create a comment
+'''
+
+a = 5
+b= 'hello world'
+
+print(a)
+print(b)
+
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+
+e = m = n ="Range"
+
+print("e:"+e)
+print("m",m)
+print("n:",n)
+
+#Unpack list
+
+fruits = ["Apple", "Banana", "Orange"]
+
+o,i,l,=fruits
+print(o)
+print(i)
+print(l)
+
+
+x = "python"
+y = "is"
+z = "awesome"
+
+print(x,y,z)
+
+
+z = "Python"
+c = "is"
+s = "awesome"
+
+print(z,c,s)
+
+
+x = 5
+u = 10
+
+print(x + u)
+
+
+x = "awesome"
+def myfunc():
+    print("Python is " + x)
+
+myfunc()
+
+
+x = "awesome"
+def myfun():
+    x = "fantastic"
+    print("Python is" + x)
+myfun()
+
+print("Python is " + x)
